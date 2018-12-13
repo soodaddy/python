f68f88ed6132a53fc1b90ee58963e54f5f022441
soodaddy <45664388+soodaddy@users.noreply.github.com>
2018-12-13 23:42:04 +0900

    2018 12 13

---
+++ test_in_git.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+num_points = 2000
+vectors_set = []
+
+for i in range(num_points):
+    if np.random.random() > 0.5:
+        vectors_set.append([np.random.normal(0.0, 0.9), np.random.normal(0.0, 0.9)])
+    else:
+        vectors_set.append([np.random.normal(3.0, 0.5), np.random.normal(1.0, 0.5)])
+
+df = pd.DataFrame({"x": [v[0] for v in vectors_set],"y": [v[1] for v in vectors_set]})
+sns.lmplot("x","y", data=df, fit_reg=False, size=6)
+plt.show()
